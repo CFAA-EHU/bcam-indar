8895d7434f2be54eac2e4a59289cb1202e158edf
Felipe Ponce <fponce@bcamath.org>
2025-07-24 14:19:59 +0200

    Move code to 'mechanical'

--- src/bcam/resonance/core/mechanical.py
+++ src/bcam/resonance/core/mechanical.py
@@ -2,9 +2,231 @@
 This is ...
 '''
 
+import logging
+
 import numpy as np
 import scipy
 
+logger = logging.getLogger(__name__)
+
+# =================================
+# Helper Functions
+# =================================
+
+def _validate_dims(M, C, K, check_symmetry=True):
+    M, C, K = np.asarray(M), np.asarray(C), np.asarray(K)
+    
+    if M.ndim != 2:
+        msg = f'Expected a 2-darray, got {M.ndim}-darray.'
+        raise ValueError(msg)
+    
+    if M.shape[0] != M.shape[1]:
+        msg = 'M must be a square array.'
+        raise ValueError(msg)
+
+    if (M.shape != C.shape) or (M.shape != K.shape):
+        msg = 'Incompatible shapes for M, C, and K.'
+        raise ValueError(msg)
+
+    if check_symmetry:
+        if not np.allclose(M, M.T):
+            msg = 'M must be symmetric.'
+            raise ValueError(msg)
+
+        if not np.allclose(K, K.T):
+            msg = 'K must be symmetric.'
+            raise ValueError(msg)
+    
+    return M, C, K
+
+# =================================
+# Amplitudes
+# =================================
+
+def _trig_fft(x):
+    '''
+    Trigonometric expansion of a real signal.
+    '''
+    N = x.shape[-1]
+    x_hat = np.fft.rfft(x, axis=-1, norm='ortho')
+    c = 2*np.ones(N//2 + 1)
+    c[0] = 1
+    if N % 2 == 0:
+        c[-1] = 1
+    x_hat = [
+        np.sqrt(c) * np.real(x_hat), # cosine part
+        -np.sqrt(2) * np.imag(x_hat)[..., 1:(N-1)//2+1] # sine part
+    ]
+    # Concatenate both parts along the last axis.
+    x_hat = np.concatenate(x_hat, axis=-1)
+    return x_hat
+
+def _trig_ifft(x):
+    '''
+    Inverse trigonometric expansion of a real signal.
+    '''
+    N = x.shape[-1]
+    c = 2*np.ones(N//2 + 1)
+    c[0] = 1
+    if N % 2 == 0:
+        c[-1] = 1
+    x_inv = x[..., :N//2+1].astype(np.complex128)/np.sqrt(c)
+    x_inv[..., 1:(N-1)//2+1] = x_inv[..., 1:(N-1)//2+1] - 1j*x[..., N//2 + 1:]/np.sqrt(2)
+    x_inv = np.fft.irfft(x_inv, N, axis=-1, norm='ortho')
+    return x_inv
+
+def _sinh_m(x):
+    eps = np.finfo(x.dtype).eps
+    y = np.where(x, x, eps)
+    return -np.exp(-y/2) * y / (2*np.sinh(y/2))
+
+def _sum_exp_weighted(a, fs: float, ns: int):
+    # TODO: add the case a = 0.
+    T = ns / fs
+    r = 1 + (1 - np.exp(a*T))/ns
+    r = r - _sinh_m(a/fs) * np.exp(a/fs) * (1 - np.exp(a*T)) / (a*T)
+    r = r * _sinh_m(a/fs) / a
+    return r
+
+def _local_matrix(freqs, fs: float, ns: int):
+    N = len(freqs)
+    r = np.zeros((2*N-1, 2*N-1))
+
+    def _mult(x, y):
+        r = x[np.newaxis, :] + y[:, np.newaxis]
+        r = np.exp(r/(2*fs)) * _sum_exp_weighted(r, fs, ns)
+        r *= (4*fs**2)*np.sinh(x[np.newaxis, :]/(2*fs)) * np.sinh(y[:, np.newaxis]/(2*fs))
+        return r
+
+    r1 = _mult(freqs, np.conj(freqs))
+    r2 = _mult(freqs, freqs)
+
+    r[:N, :N] = np.real(r1 - r2)
+    r[N:, :N] = _trig_fft(np.imag(r1 + r2).T)[..., 1:].T
+    r[:N, N:] = r[N:, :N].T
+    r[N:, N:] = _trig_fft(_trig_fft(np.real(r1 + r2))[..., 1:].T)[..., 1:]
+    r *= 0.5
+
+    return r
+
+def _reshape_injection(x, dof: int):
+    L = x.shape[-1]
+    x_ = np.zeros((dof, dof, 2*dof - 1, L), dtype=x.dtype)
+    x = x.reshape(dof*(dof+1)//2, 2*dof - 1, L)
+    x_[(np.arange(dof), np.arange(dof))] = x[:dof]
+    c = dof
+    for i in range(1, dof):
+        cn = c + dof - i
+        x_[(np.arange(dof-i), i+np.arange(dof-i))] = x[c:cn]/np.sqrt(2)
+        x_[(i+np.arange(dof-i), np.arange(dof-i))] = x[c:cn]/np.sqrt(2)
+        c = cn
+    return x_
+
+def _reshape_projection(x, dof: int):
+    L = x.shape[-1]
+    x_ = np.zeros((dof*(dof+1)//2, 2*dof - 1, L), dtype=x.dtype)
+    x_[:dof] = x[(np.arange(dof), np.arange(dof))]
+    c = dof
+    for i in range(1, dof):
+        cn = c + dof - i
+        x_[c:cn] = (x[(np.arange(dof-i), i+np.arange(dof-i))] + x[(i+np.arange(dof-i), np.arange(dof-i))]) / np.sqrt(2)
+        c = cn
+    return x_.reshape(-1, L)
+
+
+class _PreCoeff_to_Kernel(scipy.sparse.linalg.LinearOperator):
+    
+    def __init__(self, freqs, fs: float, ns: int):
+        self.freqs = freqs
+        self.fs = fs
+        self.ns = ns
+
+        dof = len(self.freqs)
+        M = dof*(dof+1)//2 * (2*dof - 1)
+        super().__init__(
+            dtype=np.float64,
+            shape=(M, M)
+        )
+
+        self._local_matrix = _local_matrix(freqs, fs, ns)
+
+    def _matmat(self, x):
+        dof = len(self.freqs)
+        r = _reshape_injection(x, dof)
+        M_local = self._local_matrix
+        r = np.einsum('lk,ijkm->ijlm', M_local, r)
+        return _reshape_projection(r, dof) / dof
+
+    def _adjoint(self):
+        return self
+
+
+class _PreCoeff():
+
+    def __init__(self, freqs, fs: float, ns: int):
+        self.freqs = freqs
+        self.fs = fs
+        self.ns = ns
+
+    @property
+    def amplitudes_(self):
+        dof = len(self.freqs)
+        x = self.raw_coeff_
+        r = np.concatenate(
+            [np.zeros((*x.shape[:2], 1)), x[..., dof:]], axis=-1)
+        r = _trig_ifft(r).astype(np.complex128)
+        r = x[..., :dof] + 1j*r
+        return r
+
+    def _rhs(self, y):
+        freqs = self.freqs
+        fs = self.fs
+        ns = self.ns
+        dof = len(freqs)
+
+        def prod(t):
+            T = ns / fs
+            t = t.reshape(1, -1)
+            freqs_ = 2*fs*np.exp(freqs/(2*fs)) * np.sinh(freqs/(2*fs))
+            r_ = np.exp(freqs[:, np.newaxis] * t) * (1 - t/T)
+            r_ *= freqs_[:, np.newaxis]
+            return r_
+
+        r = np.einsum(
+            'ijt,kt->ijk',
+            y,
+            prod(np.arange(ns)/fs)
+        )
+        r = r / (dof*fs)
+        r = np.concatenate(
+            [np.imag(r), _trig_fft(np.real(r))[..., 1:]],
+            axis=-1
+        )
+
+        return r
+
+    def fit(self, y, cg_kwargs=None):
+        dof = len(self.freqs)
+        cg_kwargs = {} if cg_kwargs is None else cg_kwargs
+        lhs = _PreCoeff_to_Kernel(self.freqs, self.fs, self.ns)
+        rhs = self._rhs(y)
+        rhs = _reshape_projection(rhs[..., np.newaxis], dof)
+        r, info = scipy.sparse.linalg.cg(lhs, rhs, **cg_kwargs)
+        if info != 0:
+            logger.warning(f'Conjugate gradient did not converge, info={info}')
+
+        r = _reshape_injection(r[..., np.newaxis], dof)
+        self.raw_coeff_ = r[..., 0]
+        return self.amplitudes_
+
+class Test(_PreCoeff):
+    '''Test class for _PreCoeff.'''
+    def __init__(self, freqs, fs: float, ns: int):
+        super().__init__(freqs, fs, ns)
+
+# =================================
+# Modal Parameters
+# =================================
 
 def modal_to_system(mode_shapes, Z):
     '''Recover system matrices from mode shapes and complex frequencies.
@@ -92,8 +314,9 @@ def system_to_modal(M, C, K):
 
 
 
-
-# === Systems ===
+# =================================
+# Systems
+# =================================
 
 def randomSystem(
         dofs,
@@ -155,6 +378,43 @@ def randomSystem(
         'mode_shapes': mode_shapes
     }
     return mechanical, modal
+
+
+class Spring:
+
+    def __init__(self, M, C, K, force=None):
+        '''Simulate a linear vibrating system.
+
+        Set up an object representing a linear system
+        to be used in JiTCODE.
+        '''
+        self.M, self.C, self.K = _validate_dims(M, C, K)
+        self.force = np.zeros(self.M.shape[0]) if force is None else np.array(force, ndmin=1)
+        if len(self.force) != self.M.shape[0]:
+            msg = f'Expected a force with {self.M.shape[0]}-DoF,\
+                got {len(self.force)} instead.'
+            raise ValueError(msg)
+
+        Minv = np.linalg.inv(self.M)
+        self._A = Minv @ C
+        self._B = Minv @ K
+        self._force = Minv @ self.force
+
+    def __iter__(self):
+        dofs = self.M.shape[0]
+        q = [y(i) for i in range(dofs)]
+        p = [y(i + dofs) for i in range(dofs)]
+        for i in range(dofs):
+            yield p[i]
+        for i in range(dofs):
+            tmp = -sum(self._B[i, j] * q[j] for j in range(dofs))
+            tmp += -sum(self._A[i, j] * p[j] for j in range(dofs))
+            tmp += self._force[i]
+            yield tmp
+
+    def __len__(self):
+        return 2 * self.M.shape[0]
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
--- src/sandbox.py
+++ src/sandbox.py
@@ -4,7 +4,6 @@
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
-import scipy.sparse
 
 from bcam.resonance import mechanical
 
@@ -23,7 +22,7 @@ z = modal['frequencies']
 
 # %%
 def _factor(freqs, fs):
-    return fs*np.exp(freqs/(2*fs)) * np.sinh(freqs/(2*fs))
+    return 2*fs*np.exp(freqs/(2*fs)) * np.sinh(freqs/(2*fs))
 
 ns, fs = 200, 100
 amplitudes = psi.reshape(DoF, 1, DoF) * psi.reshape(1, DoF, DoF)
@@ -34,220 +33,41 @@ factor = _factor(z, fs)
 r = amplitudes * factor.reshape(1, 1, DoF)
 r = r.reshape(DoF, DoF, 1, DoF)
 r = r * np.exp(z[np.newaxis, :] * t[:, np.newaxis]).reshape(1, 1, ns, DoF)
-kernel = 2*np.imag(np.sum(r, axis=-1))
+kernel = np.imag(np.sum(r, axis=-1))
 del r
 
 rng = np.random.default_rng(None)
 k_noise = kernel + rng.normal(0, 0.0, kernel.shape)
 
 # %%
-# fig, axs = plt.subplots()
-# axs.plot(t, kernel[0, 0, :], label='Kernel')
-# axs.plot(t, k_noise[0, 0, :], label='Noisy Kernel')
-# axs.legend()
-# axs.set_xlabel('Time')
-# axs.set_ylabel('Velocity')
+fig, axs = plt.subplots()
+axs.plot(t, kernel[0, 0, :], label='Kernel')
+axs.plot(t, k_noise[0, 0, :], label='Noisy Kernel')
+axs.legend()
+axs.set_xlabel('Time')
+axs.set_ylabel('Velocity')
 
-# plt.show()
-
-# # %%
-# k_hat = np.fft.rfft(kernel, axis=-1)
-# fr = np.fft.rfftfreq(kernel.shape[-1], 1/fs)
-
-# fig, axs = plt.subplots()
-# axs.plot(fr, np.abs(k_hat[0, 0, :]))
-# axs.set_xlabel('Frequency')
-
-# plt.show()
+plt.show()
 
 # %%
-def _trig_fft(x):
-    N = x.shape[-1]
-    x_hat = np.fft.rfft(x, axis=-1, norm='ortho')
-    c = 2*np.ones(N//2 + 1)
-    c[0] = 1
-    if N % 2 == 0:
-        c[-1] = 1
-    x_hat = [
-        np.sqrt(c) * np.real(x_hat),
-        -np.sqrt(2) * np.imag(x_hat)[..., 1:(N-1)//2+1]
-    ]
-    # Concatenate both parts along the last axis.
-    x_hat = np.concatenate(x_hat, axis=-1)
-    return x_hat
+k_hat = np.fft.rfft(kernel, axis=-1)
+fr = np.fft.rfftfreq(kernel.shape[-1], 1/fs)
 
-def _trig_ifft(x):
-    N = x.shape[-1]
-    c = 2*np.ones(N//2 + 1)
-    c[0] = 1
-    if N % 2 == 0:
-        c[-1] = 1
-    x_inv = x[..., :N//2+1].astype(np.complex128)/np.sqrt(c)
-    x_inv[..., 1:(N-1)//2+1] = x_inv[..., 1:(N-1)//2+1] - 1j*x[..., N//2 + 1:]/np.sqrt(2)
-    x_inv = np.fft.irfft(x_inv, N, axis=-1, norm='ortho')
-    return x_inv
+fig, axs = plt.subplots()
+axs.plot(fr, np.abs(k_hat[0, 0, :]))
+axs.set_xlabel('Frequency')
 
-# y = rng.normal(size=(5))
-# y_hat = _trig_fft(y)
-# y_ = _trig_ifft(y_hat)
-
-def _sinh_m(x):
-    eps = np.finfo(x.dtype).eps
-    y = np.where(x, x, eps)
-    return np.exp(-y/2) * y / (2*np.sinh(y/2))
-
-def _sum_exp_weighted(a, fs: float, ns: int):
-    # TODO: add the case a = 0.
-    T = ns / fs
-    r = 1 + (1 - np.exp(a*T))/ns
-    r = r - _sinh_m(a/fs) * np.exp(a/fs) * (np.exp(a*T)-1) / (a*T)
-    r = r * _sinh_m(a/fs) / a
-    return r
-
-def _local_matrix(freqs, fs: float, ns: int):
-    N = len(freqs)
-    r = np.zeros((2*N-1, 2*N-1))
-
-    def _mult(x, y):
-        r = x[np.newaxis, :] + y[:, np.newaxis]
-        r = np.exp(r/(2*fs)) * _sum_exp_weighted(r, fs, ns)
-        r *= np.sinh(x[np.newaxis, :]/(2*fs)) * np.sinh(y[:, np.newaxis]/(2*fs))
-        r *= fs**2
-        return r
-
-    r1 = _mult(freqs, np.conj(freqs))
-    r2 = _mult(freqs, freqs)
-
-    r[:N, :N] = np.real(r1 - r2)
-    r[N:, :N] = _trig_fft(np.imag(r1 + r2).T)[..., 1:].T
-    r[:N, N:] = r[N:, :N].T
-    r[N:, N:] = _trig_fft(_trig_fft(np.real(r1 + r2))[..., 1:].T)[..., 1:]
-    r *= 2.
-
-    return r
-
-def _reshape_injection(x, dof: int):
-    L = x.shape[-1]
-    x_ = np.zeros((dof, dof, 2*dof - 1, L), dtype=x.dtype)
-    x = x.reshape(dof*(dof+1)//2, 2*dof - 1, L)
-    x_[(np.arange(dof), np.arange(dof))] = x[:dof]
-    c = dof
-    for i in range(1, dof):
-        cn = c + dof - i
-        x_[(np.arange(dof-i), i+np.arange(dof-i))] = x[c:cn]/np.sqrt(2)
-        x_[(i+np.arange(dof-i), np.arange(dof-i))] = x[c:cn]/np.sqrt(2)
-        c = cn
-    return x_
-
-def _reshape_projection(x, dof: int):
-    L = x.shape[-1]
-    x_ = np.zeros((dof*(dof+1)//2, 2*dof - 1, L), dtype=x.dtype)
-    x_[:dof] = x[(np.arange(dof), np.arange(dof))]
-    c = dof
-    for i in range(1, dof):
-        cn = c + dof - i
-        x_[c:cn] = (x[(np.arange(dof-i), i+np.arange(dof-i))] + x[(i+np.arange(dof-i), np.arange(dof-i))]) / np.sqrt(2)
-        c = cn
-    return x_.reshape(-1, L)
-
-# x = rng.normal(size=((DoF*(DoF+1)//2) * (2*DoF - 1), 2))
-# x_ = _reshape_injection(x, DoF)
-# x2 = _reshape_projection(x_, DoF)
-
-class _PreCoeff_to_Kernel(scipy.sparse.linalg.LinearOperator):
-    
-    def __init__(self, freqs, fs: float, ns: int):
-        self.freqs = freqs
-        self.fs = fs
-        self.ns = ns
-
-        dof = len(self.freqs)
-        M = dof*(dof+1)//2 * (2*dof - 1)
-        super().__init__(
-            dtype=np.float64,
-            shape=(M, M)
-        )
-
-        self._local_matrix = _local_matrix(freqs, fs, ns)
-
-    def _matmat(self, x):
-        dof = len(self.freqs)
-        r = _reshape_injection(x, dof)
-        M_local = self._local_matrix
-        r = np.einsum('lk,ijkm->ijlm', M_local, r)
-        return _reshape_projection(r, dof) / dof
-
-    def _adjoint(self):
-        return self
-
-
-class _PreCoeff():
-
-    def __init__(self, freqs, fs: float, ns: int):
-        self.freqs = freqs
-        self.fs = fs
-        self.ns = ns
-
-    @property
-    def amplitudes_(self):
-        dof = len(self.freqs)
-        x = self.raw_coeff_
-        r = np.concatenate(
-            [np.zeros((*x.shape[:2], 1)), x[..., dof:]], axis=-1)
-        r = _trig_ifft(r).astype(np.complex128)
-        r = x[..., :dof] + 1j*r
-        return r
-
-    def _rhs(self, y):
-        freqs = self.freqs
-        fs = self.fs
-        ns = self.ns
-        dof = len(freqs)
-
-        def _prod(t):
-            T = ns / fs
-            t = t.reshape(1, -1)
-            freqs_ = fs*np.exp(freqs/(2*fs)) * np.sinh(freqs/(2*fs))
-            r_ = np.exp(freqs[:, np.newaxis] * t) * (1 - t/T)
-            r_ *= freqs_[:, np.newaxis]
-            return r_
-
-        r = np.einsum(
-            'ijt,kt->ijk',
-            y,
-            _prod(np.arange(ns)/fs)
-        )
-        r = 2*r / (dof*fs)
-        r = np.concatenate(
-            [np.imag(r), _trig_fft(np.real(r))[..., 1:]],
-            axis=-1
-        )
-
-        return r
-
-    def fit(self, y, cg_kwargs=None):
-        dof = len(self.freqs)
-        cg_kwargs = {} if cg_kwargs is None else cg_kwargs
-        lhs = _PreCoeff_to_Kernel(self.freqs, self.fs, self.ns)
-        rhs = self._rhs(y)
-        rhs = _reshape_projection(rhs[..., np.newaxis], dof)
-        r, info = scipy.sparse.linalg.cg(lhs, rhs, **cg_kwargs)
-        # if info != 0:
-        #     logger.warning(f'Conjugate gradient did not converge, info={info}')
-
-        r = _reshape_injection(r[..., np.newaxis], dof)
-        self.raw_coeff_ = r[..., 0]
-        return self.amplitudes_
+plt.show()
 
 # %%
-preCoeff = _PreCoeff(z, fs, ns)
+preCoeff = mechanical.Test(z, fs, ns)
 amplitudes_fit = preCoeff.fit(k_noise)
 
 # print(
-#     np.max(np.linalg.norm(amplitudes + amplitudes_fit, axis=-1)/np.linalg.norm(amplitudes, axis=-1))
+#     np.max(np.linalg.norm(amplitudes - amplitudes_fit, axis=-1)/np.linalg.norm(amplitudes, axis=-1))
 # )
 
-i, j = 2, 1
+i, j = 2, 2
 fig, axs = plt.subplots(nrows=2)
 axs[0].set_title('Real')
 axs[0].plot(amplitudes[i, j, :].real, label='Amplitudes')
