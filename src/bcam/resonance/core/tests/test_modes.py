fead1d657131b3a4aeed7049d6ff910dc5477943
felipe <feponcev@bcamath.org>
2025-08-13 12:19:07 +0200

    Test fitting of mechanical amplitudes

--- src/bcam/resonance/core/tests/test_modes.py
+++ src/bcam/resonance/core/tests/test_modes.py
@@ -1,3 +1,4 @@
+from xml.parsers.expat import model
 import pytest
 
 import numpy as np
@@ -36,35 +37,68 @@ class TestModeMap:
         test_obj = -np.imag(psi / (freqs * np.imag(freqs))[np.newaxis, :] @ psi.T)
         scipy.linalg.cholesky(test_obj, lower=False, overwrite_a=True)
 
-def test_amplitudes():
-    rng = np.random.default_rng(12345)
-    dof = 4
-    freqs = -rng.uniform(1, 3, dof) + 2j*np.pi*rng.uniform(1, 50, dof)
-    ns, fs = 200, 100
-    n_out, n_in = 3, 2
+class TestAmplitudes:
 
-    def kernel(t, a, freqs):
+    @staticmethod
+    def kernel(ns, fs, a, freqs):
+        dof = len(freqs)
+        t = np.arange(ns) / fs
         K = 2*fs*np.exp(freqs/(2*fs)) * np.sinh(freqs/(2*fs))
         K = a * K.reshape(1, 1, dof)
         K = np.expand_dims(K, axis=2)
         K = K * np.exp(freqs[np.newaxis, :] * t[:, np.newaxis]).reshape(1, 1, ns, dof)
         K = np.imag(np.sum(K, axis=-1))
         return K
+    
+    def test_amps(self):
+        rng = np.random.default_rng(12345)
+        dof = 4
+        freqs = -rng.uniform(1, 3, dof) + 2j*np.pi*rng.uniform(1, 50, dof)
+        ns, fs = 200, 100
+        n_out, n_in = 3, 2
 
-    t = np.arange(ns) / fs
+        amps = rng.normal(scale=1, size=(n_out, n_in, dof)).astype(np.complex128)
+        amps += 1j * rng.normal(scale=.1, size=(n_out, n_in, dof))
+        amps[:n_in] = 0.5 * (amps[:n_in] + amps[:n_in].transpose((1, 0, 2)))
+        data = self.kernel(ns, fs, amps, freqs)
 
-    amps = rng.normal(scale=1, size=(n_out, n_in, dof)).astype(np.complex128)
-    amps += 1j * rng.normal(scale=.1, size=(n_out, n_in, dof))
-    amps[:n_in] = 0.5 * (amps[:n_in] + amps[:n_in].transpose((1, 0, 2)))
-    data = kernel(t, amps, freqs)
+        model = mechanical.Amplitudes(
+            freqs=freqs,
+            fs=fs,
+            ns=ns,
+            n_out=n_out,
+            n_in=n_in
+        )
+        amps_fit = model.fit(data, penalty=0.)
+        error = np.linalg.norm(amps_fit - amps, axis=-1) / np.linalg.norm(amps, axis=-1)
+        assert np.max(error) < 1e-2
 
-    model = mechanical.Amplitudes(
-        freqs=freqs,
-        fs=fs,
-        ns=ns,
-        n_out=n_out,
-        n_in=n_in
-    )
-    amps_fit = model.fit(data, penalty=0.)
-    error = np.linalg.norm(amps_fit - amps, axis=-1) / np.linalg.norm(amps, axis=-1)
-    assert np.max(error) < 1e-2
+    def test_mech_amps(self):
+        dof = 4
+        modal = mechanical.randomSystem(
+            dof,
+            mass_range=(1, 2),
+            damping_range=(0.02, 0.05),
+            freqs_range=(2 * np.pi * 1, 2 * np.pi * 20),
+            damping_type='non-proportional',
+            seed=123456)[1]
+        psi = modal['mode_shapes']
+        freqs = modal['frequencies']
+        ns, fs = 210, 100
+        n_out, n_in = 3, 2
+
+        amps = psi.reshape(dof, 1, dof) * psi.reshape(1, dof, dof)
+        amps = amps * (1/np.imag(freqs)).reshape(1, 1, dof)
+        amps = amps[:n_out, :n_in]
+        data = self.kernel(ns, fs, amps, freqs)
+
+        model = mechanical.Amplitudes(
+            freqs=freqs,
+            fs=fs,
+            ns=ns,
+            n_out=n_out,
+            n_in=n_in,
+            a_type='mechanical')
+        amps_fit = model.fit(data)
+        error = np.linalg.norm(amps_fit - amps, axis=-1) / np.linalg.norm(amps, axis=-1)
+        assert np.max(error) < 1e-2
